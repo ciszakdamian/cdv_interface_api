8e5291304751a80a8ee24533eb77ea4e9360cf90
ciszakdamian <ciszakdamian@gmail.com>
2019-10-12 01:14:07 +0200

    final 2

--- 2.py
+++ 2.py
@@ -1,11 +1,10 @@
 import string
-
 def translate(text):
     decode = ""
     for char in text:
         code = ord(char)
 
-        if 97 >= code <= 120:
+        if 97 > code <= 120:
             number = code
         else:
             if code == 121 or code == 122:
@@ -21,15 +20,4 @@ def translate(text):
 
 print(translate("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."))
 
-print(translate("a"))
-
-print(string.ascii_lowercase)
-print(translate(string.ascii_lowercase))
-
-intab = "abcdefghijklmnopqrstuvwxyz"
-outtab = "adefghijklmnopqrstuvwxyzab"
-trantab = str.maketrans(intab, outtab)
-
-str = "map"
-print(str.translate(trantab))
-
+print(translate("map"))
