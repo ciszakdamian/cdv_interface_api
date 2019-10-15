05ac14c5aa6a067d96d6a42d36193072b9202259
Damian <ciszakdamian@gmail.com>
2019-10-15 17:46:26 +0200

    add 3 50%

--- 3/3.py
+++ 3/3.py
@@ -1,2 +1,24 @@
 #http://www.pythonchallenge.com/pc/def/equality.html
 
+import string
+
+#load data from file
+file = open("data.txt")
+data = file.read()
+
+#generate ascci lowercase code
+alphabet = string.ascii_lowercase
+
+lowercase_ascii = []
+for char in alphabet:
+    lowercase_ascii.append(ord(char))
+
+print(lowercase_ascii)
+
+#find lowercase later
+#refex ([A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z])
+lowercase = ''
+
+
+
+print(lowercase)
