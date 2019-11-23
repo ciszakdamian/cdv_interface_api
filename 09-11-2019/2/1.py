670cf969ae1dd8ef2ea714aea4ba1cad4476a016
ciszakdamian <ciszakdamian@gmail.com>
2019-11-23 02:28:53 +0100

    add new

--- 09-11-2019/2/1.py
+++ 09-11-2019/2/1.py
@@ -1,2 +1,11 @@
 import numpy as np
-a = np.matrix([1, 2, 3])
+
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[1, 1], [1, 1]])
+
+print('A = '+str(a))
+print('B = '+str(b))
+
+
+print('A + B = '+str(np.add(a, b)))
+print('A * B = '+str(np.matmul(a, b)))
